c1274cbef6098e476d1fc6c21d7132d3abf37730
rfilguei <r.filgueira@epcc.ed.ac.uk>
2021-10-19 17:08:05 +0100

    postprocessing

--- NLS_EB/postprocess_scripts/postprocess.sh
+++ NLS_EB/postprocess_scripts/postprocess.sh
@@ -4,25 +4,13 @@ set -x
 
 python process_metadata_eb.py
 
-###### Cleaning and Creaating the UPDATED TXT FILES #####
-python post_process_eb.py results_eb_1_edition 
-python post_process_eb.py results_eb_2_edition 
-python post_process_eb.py results_eb_3_edition 
-python post_process_eb.py results_eb_4_edition 
-python post_process_eb.py results_eb_5_edition 
-python post_process_eb.py results_eb_6_edition 
-python post_process_eb.py results_eb_7_edition 
-python post_process_eb.py results_eb_8_edition 
-python post_process_eb.py results_eb_4_5_6_suplement 
-
-###### Create dataframes FILES ########
-python dataframe_eb.py results_eb_1_edition_updated 
-python dataframe_eb.py results_eb_2_edition_updated
-python dataframe_eb.py results_eb_3_edition_updated 
-python dataframe_eb.py results_eb_4_edition_updated 
-python dataframe_eb.py results_eb_5_edition_updated 
-python dataframe_eb.py results_eb_6_edition_updated 
-python dataframe_eb.py results_eb_7_edition_updated 
-python dataframe_eb.py results_eb_8_edition_updated 
-python dataframe_eb.py results_eb_4_5_6_suplement_updated
-
+###### Cleaning and Creating the UPDATED TXT FILES and DATAFRAMES #####
+python postprocess_eb.py results_eb_1_edition 
+python postprocess_eb.py results_eb_2_edition 
+python postprocess_eb.py results_eb_3_edition 
+python postprocess_eb.py results_eb_4_edition 
+python postprocess_eb.py results_eb_5_edition 
+python postprocess_eb.py results_eb_6_edition 
+python postprocess_eb.py results_eb_7_edition 
+python postprocess_eb.py results_eb_8_edition 
+python postprocess_eb.py results_eb_4_5_6_suplement 
