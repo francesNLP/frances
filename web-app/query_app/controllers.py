e9011c6579465baa6da03ffb80e25c59ce812211
rfilguei <r.filgueira@epcc.ed.ac.uk>
2021-11-08 18:30:39 +0000

    updates

--- web-app/query_app/controllers.py
+++ web-app/query_app/controllers.py
@@ -40,6 +40,64 @@ def get_editions():
     return clean_r
 
 
+def get_numberOfVolumes(uri):
+    sparql = SPARQLWrapper("http://localhost:3030/edition1st/sparql")
+    query="""
+    PREFIX eb: <https://w3id.org/eb#>
+    SELECT (COUNT (DISTINCT ?v) as ?count)
+        WHERE {
+            %s eb:hasPart ?v.
+    	    ?v ?b ?c
+    }
+    """ % (uri)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return results["results"]["bindings"][0]["count"]["value"]
+
+
+def get_editions_details(uri=None):
+    sparql = SPARQLWrapper("http://localhost:3030/edition1st/sparql")
+    if not uri:
+       uri="<https://w3id.org/eb/i/Edition/992277653804341>"
+    query="""
+    PREFIX eb: <https://w3id.org/eb#>
+    SELECT ?genre ?publicationYear ?num ?title ?subtitle ?printedAt ?physicalDescription ?mmsid ?shelfLocator ?numberOfVolumes  WHERE {
+           %s eb:publicationYear ?publicationYear ;
+              eb:number ?num;
+              eb:title ?title;
+              eb:subtitle ?subtitle ;
+              eb:printedAt ?printedAt;
+              eb:physicalDescription ?physicalDescription;
+              eb:mmsid ?mmsid;
+              eb:shelfLocator ?shelfLocator;
+              eb:genre ?genre. 
+    }
+    """ % (uri)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    clean_r={}
+    for r in results["results"]["bindings"]:
+        clean_r["publicationYear"]=r["publicationYear"]["value"]
+        clean_r["num"]=r["num"]["value"]
+        clean_r["uri"]=uri
+        clean_r["title"]=r["title"]["value"]
+        if "subtitle" in r:
+            clean_r["subtitle"]=r["subtitle"]["value"]
+        else:
+            clean_r["subtitle"]="No value"
+        clean_r["printedAt"]=r["printedAt"]["value"]
+        clean_r["physicalDescription"]=r["physicalDescription"]["value"]
+        clean_r["mmsid"]=r["mmsid"]["value"]
+        clean_r["shelfLocator"]=r["shelfLocator"]["value"]
+        clean_r["genre"]=r["genre"]["value"]
+        clean_r["language"]="English"
+        clean_r["numberOfVolumes"]=get_numberOfVolumes(uri)
+
+    return clean_r
+
+
 def get_definition(term=None):
     term=term.upper()
     query1="""
@@ -91,9 +149,15 @@ def rs():
                                                         term=term
                                                         )
 
-@app.route("/eb_details")
+@app.route("/eb_details", methods=['GET', 'POST'])
 def eb_details():
     edList=get_editions()
+    if 'edition_selection' in request.form:
+        uri_raw=request.form.get('edition_selection')
+        uri="<"+uri_raw+">"
+        print("Uri is %s" %uri)
+        ed_r=get_editions_details(uri)
+        return render_template('eb_details.html', edList=edList, ed_r=ed_r)
     return render_template('eb_details.html', edList=edList)
 
 @app.route("/evolution_terms", methods=["GET"])
