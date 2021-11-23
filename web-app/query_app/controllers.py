e12e329034d47a9a45ca28eeb693e8a4085492cd
rfilguei <r.filgueira@epcc.ed.ac.uk>
2021-11-23 20:14:46 +0000

    updates

--- web-app/query_app/controllers.py
+++ web-app/query_app/controllers.py
@@ -7,12 +7,15 @@ from flask_paginate import Pagination, get_page_parameter
 import itertools
 from itertools import islice
 from sklearn.metrics.pairwise import cosine_similarity
-from .utils import calculating_similarity_text
+from .utils import calculating_similarity_text, get_topic_name
+
 import numpy as np
 import os
 import pickle
 from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
+from bertopic import BERTopic
+
 ########
 input_path="/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models"
 text_embeddings = np.load('/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models/embeddings_1ed.npy')
@@ -32,7 +35,19 @@ with open (os.path.join(input_path, 'terms_info_1ed.txt'), 'rb') as fp3:
 with open (os.path.join(input_path, 'uris_1ed.txt'), 'rb') as fp4:
     uris = pickle.load(fp4)
 
+with open (os.path.join(input_path, 'topics_1ed.txt'), 'rb') as fp5:
+    topics = pickle.load(fp5)
+
+with open (os.path.join(input_path, 't_names_1ed.txt'), 'rb') as fp6:
+    t_names = pickle.load(fp6)
+
+with open (os.path.join(input_path, 'topics_names_1ed.txt'), 'rb') as fp7:
+    topics_names = pickle.load(fp7)
+
 model = SentenceTransformer('bert-base-nli-mean-tokens')
+
+topic_model = BERTopic.load("/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models/BerTopic_Model_1ed") 
+
 ######
 
 @app.route("/", methods=["GET"])
@@ -43,7 +58,7 @@ def home_page():
 @app.route("/term_search",  methods=['GET', 'POST'])
 def term_search(termlink=None):
     
-    headers=["Year", "Edition", "Volume", "Start Page", "End Page", "Term Type", "Definition", "Related Terms"]
+    headers=["Year", "Edition", "Volume", "Start Page", "End Page", "Term Type", "Definition", "Related Terms", "Topic Modelling"]
     if request.method == "POST":
         if "search" in request.form:
             term = request.form["search"]
@@ -60,6 +75,25 @@ def term_search(termlink=None):
     if not term:
         term = "ABACISCUS"
     results =get_definition(term)
+    topics_vis=[]
+    for key, value in results.items():
+        index_uri=uris.index(key)
+        topic_name = topics_names[index_uri]
+        if topics[index_uri] not in topics_vis:
+            topics_vis.append(topics[index_uri])
+        #topic_name = get_topic_name(index_uri, topics, topic_model)
+        value.append(topic_name)
+    if len(topics_vis) >= 1:
+        fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
+        bar_plot = fig1.to_json() 
+    else:
+        bar_plot=None
+    if len(topics_vis) >= 2:
+        fig2=topic_model.visualize_heatmap(topics_vis)
+        heatmap_plot = fig2.to_json()
+    else:
+        heatmap_plot=None
+ 
     page = int(request.args.get("page", 1))
     page_size=2
     per_page = 2
@@ -70,7 +104,7 @@ def term_search(termlink=None):
     return render_template("results.html", results=results_for_render,
                                            pagination = pagination, 
                                            headers=headers,
-                                           term=term)
+                                           term=term, bar_plot=bar_plot, heatmap_plot=heatmap_plot)
        
 
 @app.route("/eb_details",  methods=['GET', 'POST'])
@@ -144,21 +178,46 @@ def similar_terms(termlink=None):
                 uri_raw="https://w3id.org/eb/i/Article/992277653804341_144133901_ABACISCUS_0"
             else:
                 uri_raw=""
-                results=calculating_similarity_text(data_similar,text_embeddings, model, terms_info, documents,uris)              
+                results, topics_vis=calculating_similarity_text(data_similar,text_embeddings, model, terms_info, documents,uris, topics_names,topics)              
+                if len(topics_vis) >= 1:
+                    fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
+                    bar_plot = fig1.to_json() 
+                else:
+                    bar_plot=None
+                if len(topics_vis) >= 2:
+                    fig2=topic_model.visualize_heatmap(topics_vis)
+                    heatmap_plot = fig2.to_json()
+                else:
+                    heatmap_plot=None
             if uri_raw!="":
                 uri="<"+uri_raw+">"
                 term, definition, enum, year, vnum  =get_document(uri)
                 index_uri=uris.index(uri_raw)
+                t_name=topics_names[index_uri]
                 results={}
+                topics_vis=[]
                 for i in range(-2, -12, -1):
                     similar_index=similarities_sorted[index_uri][i]
                     rank=similarities[index_uri][similar_index]
-                    results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], rank]
+                    topic_name = topics_names[similar_index]
+                    if topics[similar_index] not in topics_vis:
+                        topics_vis.append(topics[similar_index])
+                    results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank]
+                if len(topics_vis) >= 1:
+                    fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
+                    bar_plot = fig1.to_json()
+                else:
+                    bar_plot=None
+                if len(topics_vis) >= 2:
+                    fig2=topic_model.visualize_heatmap(topics_vis)
+                    heatmap_plot = fig2.to_json()
+                else:
+                    heatmap_plot=None
     
             if uri_raw == "":
-                return render_template('similar.html', results=results)
+                return render_template('similar.html', results=results, bar_plot=bar_plot, heatmap_plot=heatmap_plot)
             else:
-                return render_template('similar.html', results=results, term=term, definition=definition, uri=uri_raw, enum=enum, year=year, vnum=vnum)
+                return render_template('similar.html', results=results, term=term, definition=definition, uri=uri_raw, enum=enum, year=year, vnum=vnum, t_name=t_name, bar_plot=bar_plot, heatmap_plot=heatmap_plot)
     
     termlink  = request.args.get('termlink', None)
     termtype  = request.args.get('termtype', None)
@@ -169,13 +228,59 @@ def similar_terms(termlink=None):
         term, definition, enum, year, vnum  =get_document(uri)
         uri_raw=uri.replace("<","").replace(">","")
         index_uri=uris.index(uri_raw)
+        t_name=topics_names[index_uri]
         results={}
+        topics_vis=[]
         for i in range(-2, -12, -1):
             similar_index=similarities_sorted[index_uri][i]
             rank=similarities[index_uri][similar_index]
-            results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], rank]
-        return render_template('similar.html', results=results, term=term, definition=definition, uri=uri, enum=enum, year=year, vnum=vnum)
+            topic_name = topics_names[similar_index]
+            if topics[similar_index] not in topics_vis:
+                topics_vis.append(topics[similar_index])
+            results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank]
+
+        if len(topics_vis) >= 1:
+            fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
+            bar_plot = fig1.to_json() 
+        else:
+            bar_plot = None
+        if len(topics_vis) >= 2:
+            fig2=topic_model.visualize_heatmap(topics_vis)
+            heatmap_plot = fig2.to_json()
+        else:
+            heatmap_plot = None
+        return render_template('similar.html', results=results, term=term, definition=definition, uri=uri, enum=enum, year=year, vnum=vnum, t_name=t_name, bar_plot=bar_plot, heatmap_plot=heatmap_plot)
     return render_template('similar.html')
+
+@app.route("/topic_modelling", methods=["GET", "POST"])
+def topic_modelling(topic_name=None):
+    if request.method == "POST":
+        if 'topic_name' in request.form:
+            topic_name=request.form.get('topic_name')
+            if topic_name=="":
+                topic_name="6_see_dialling_mahometans_bildge"
+            indices = [i for i, x in enumerate(topics_names) if x == topic_name]
+            results={}
+            for t_i in indices:
+               results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i]]
+            num_results=len(indices)
+            first_topic=topics[indices[0]]
+            fig1=topic_model.visualize_barchart([first_topic], n_words=10)
+            bar_plot = fig1.to_json()
+            return render_template('topic_modelling.html', topic_name=topic_name, results=results, bar_plot=bar_plot, num_results=num_results) 
+    topic_name  = request.args.get('topic_name', None)
+    if topic_name!=None:
+        indices = [i for i, x in enumerate(topics_names) if x == topic_name]
+        results={}
+        for t_i in indices:
+            results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i]]
+        num_results=len(indices)
+        first_topic=topics[indices[0]]
+        fig1=topic_model.visualize_barchart([first_topic], n_words=10)
+        bar_plot = fig1.to_json()
+        return render_template('topic_modelling.html', topic_name=topic_name, results=results, bar_plot=bar_plot, num_results=num_results) 
+            
+    return render_template('topic_modelling.html')
 
 @app.route("/topic_summarization", methods=["GET"])
 def topic_summarization():
