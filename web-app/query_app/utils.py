d5a9648c7ec2dea1c8ae31ec2d95387613659a68
rfilguei <r.filgueira@epcc.ed.ac.uk>
2021-11-23 20:15:15 +0000

    .

--- web-app/query_app/utils.py
+++ web-app/query_app/utils.py
@@ -1,14 +1,33 @@
 from tqdm import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
 
-def calculating_similarity_text(definition, text_embeddings, model, terms_info, documents, uris):
+
+def get_topic_name(index_uri, topics, topic_model):
+    topic_num=topics[index_uri]
+    topic_info=topic_model.get_topic(topic_num)
+    topic_name="" 
+    #lets get the first 4 elements
+    cont = 0
+    for i in topic_info:
+        topic_name=topic_name+"_"+i[0]
+        cont=cont+1
+        if cont == 4:
+           break
+    topic_name=str(topic_num)+topic_name
+    return topic_name
+
+def calculating_similarity_text(definition, text_embeddings, model, terms_info, documents, uris, topics_names, topics):
     definition_embedding= model.encode(definition, batch_size = 8, show_progress_bar = True)
     similarities=cosine_similarity( [definition_embedding], text_embeddings)
     similarities_sorted = similarities.argsort()
     results={}
+    topics_vis=[]
     for i in range(-1, -12, -1):
         similar_index=similarities_sorted[0][i]
         rank=similarities[0][similar_index]
-        results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], rank]
-    return results
+        topic_name = topics_names[similar_index]
+        if topics[similar_index] not in topics_vis:
+            topics_vis.append(topics[similar_index])
+        results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank]
+    return results, topics_vis
 
