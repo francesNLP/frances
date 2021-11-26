e5898d999f418c043ed31d80453d6a52a54d5970
rfilguei <r.filgueira@epcc.ed.ac.uk>
2021-11-26 18:33:44 +0000

    improvments

--- web-app/query_app/controllers.py
+++ web-app/query_app/controllers.py
@@ -161,132 +161,138 @@ def visualization_resources(termlink=None, termtype=None):
         else:
             return render_template('visualization_resources.html')
 
+
+@app.route("/similar", methods=["GET"])
+def similar():
+    return render_template('similar.html')
+
+
 @app.route("/similar_terms", methods=["GET", "POST"])
 def similar_terms(termlink=None):
-    if request.method == "POST":
-        if 'resource_uri' in request.form:
-            data_similar=request.form.get('resource_uri')
-            if "https://" in data_similar or "w3id" in data_similar:
-                uri_raw=data_similar.strip().replace("<","").replace(">","")
-            elif data_similar == "":
-                uri_raw="https://w3id.org/eb/i/Article/992277653804341_144133901_ABACISCUS_0"
-            else:
-                uri_raw=""
-                results, topics_vis=calculating_similarity_text(data_similar,text_embeddings, model, terms_info, documents,uris, topics_names,topics, sentiment_terms)              
-                if len(topics_vis) >= 1:
-                    fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
-                    bar_plot = fig1.to_json() 
-                else:
-                    bar_plot=None
-                if len(topics_vis) >= 2:
-                    fig2=topic_model.visualize_heatmap(topics_vis)
-                    heatmap_plot = fig2.to_json()
-                else:
-                    heatmap_plot=None
-            if uri_raw!="":
-                uri="<"+uri_raw+">"
-                term, definition, enum, year, vnum  =get_document(uri)
-                index_uri=uris.index(uri_raw)
-                t_name=topics_names[index_uri]
-                score='%.2f'%sentiment_terms[index_uri][0]['score']
-                t_sentiment = sentiment_terms[index_uri][0]['label']+"_"+score
-                results={}
-                topics_vis=[]
-                for i in range(-2, -12, -1):
-                    similar_index=similarities_sorted[index_uri][i]
-                    rank=similarities[index_uri][similar_index]
-                    score='%.2f'%sentiment_terms[similar_index][0]['score']
-                    sentiment = sentiment_terms[similar_index][0]['label']+"_"+score
-                    topic_name = topics_names[similar_index]
-                    if topics[similar_index] not in topics_vis:
-                        topics_vis.append(topics[similar_index])
-                    results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank, sentiment]
-                if len(topics_vis) >= 1:
-                    fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
-                    bar_plot = fig1.to_json()
-                else:
-                    bar_plot=None
-                if len(topics_vis) >= 2:
-                    fig2=topic_model.visualize_heatmap(topics_vis)
-                    heatmap_plot = fig2.to_json()
-                else:
-                    heatmap_plot=None
-    
-            if uri_raw == "":
-                return render_template('similar.html', results=results, bar_plot=bar_plot, heatmap_plot=heatmap_plot)
-            else:
-                return render_template('similar.html', results=results, term=term, definition=definition, uri=uri_raw, enum=enum, year=year, vnum=vnum, t_name=t_name, bar_plot=bar_plot, heatmap_plot=heatmap_plot, t_sentiment=t_sentiment)
-    
+    uri=""
+    uri_raw=""
+    data_similar=""
+    topics_vis=[]
     termlink  = request.args.get('termlink', None)
     termtype  = request.args.get('termtype', None)
     if termlink!=None:
         if ">" in termlink:
             termlink=termlink.split(">")[0]
         uri="<https://w3id.org/eb/i/"+termtype+"/"+termlink+">"
-        term, definition, enum, year, vnum  =get_document(uri)
         uri_raw=uri.replace("<","").replace(">","")
+        session['uri_raw'] = uri_raw
+        session['uri'] = uri
+        session["data_similar"] = ""
+
+    elif 'resource_uri' in request.form:
+        data_similar=request.form.get('resource_uri')
+        if "https://" in data_similar or "w3id" in data_similar:
+            uri_raw=data_similar.strip().replace("<","").replace(">","")
+        elif data_similar == "":
+            uri_raw="https://w3id.org/eb/i/Article/992277653804341_144133901_ABACISCUS_0"
+        else:
+            uri_raw=""
+            session['uri_raw'] = "free_search"
+        if uri_raw!="":
+            uri="<"+uri_raw+">"
+            session['uri_raw'] = uri_raw
+        session['uri'] = uri
+        session["data_similar"] = data_similar
+
+    if not uri:
+        uri=session.get('uri')      
+        uri_raw=session.get('uri_raw')
+        data_similar = session.get('data_similar')
+      
+    if "free_search" in uri_raw:
+        results, topics_vis=calculating_similarity_text(data_similar,text_embeddings, model, terms_info, documents,uris, topics_names,topics, sentiment_terms)
+              
+    else:
+        term, definition, enum, year, vnum  =get_document(uri)
         index_uri=uris.index(uri_raw)
         t_name=topics_names[index_uri]
         score='%.2f'%sentiment_terms[index_uri][0]['score']
         t_sentiment = sentiment_terms[index_uri][0]['label']+"_"+score
         results={}
         topics_vis=[]
-        for i in range(-2, -12, -1):
+        for i in range(-2, -22, -1):
             similar_index=similarities_sorted[index_uri][i]
             rank=similarities[index_uri][similar_index]
             score='%.2f'%sentiment_terms[similar_index][0]['score']
             sentiment = sentiment_terms[similar_index][0]['label']+"_"+score
             topic_name = topics_names[similar_index]
             if topics[similar_index] not in topics_vis:
-                topics_vis.append(topics[similar_index])
+               topics_vis.append(topics[similar_index])
             results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank, sentiment]
-
-        if len(topics_vis) >= 1:
-            fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
-            bar_plot = fig1.to_json() 
-        else:
-            bar_plot = None
-        if len(topics_vis) >= 2:
-            fig2=topic_model.visualize_heatmap(topics_vis)
-            heatmap_plot = fig2.to_json()
-        else:
-            heatmap_plot = None
-        return render_template('similar.html', results=results, term=term, definition=definition, uri=uri, enum=enum, year=year, vnum=vnum, t_name=t_name, bar_plot=bar_plot, heatmap_plot=heatmap_plot, t_sentiment=t_sentiment)
-    return render_template('similar.html')
+    if len(topics_vis) >= 1:
+        fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
+        bar_plot = fig1.to_json()
+    else:
+        bar_plot=None
+    if len(topics_vis) >= 2:
+        fig2=topic_model.visualize_heatmap(topics_vis)
+        heatmap_plot = fig2.to_json()
+    else:
+        heatmap_plot=None
+             
+    #### Pagination ###  
+    page = int(request.args.get("page", 1))
+    page_size=10
+    per_page = 10
+    offset = (page-1) * per_page
+    limit = offset+per_page
+    results_for_render=dict(islice(results.items(),offset, limit))
+    pagination = Pagination(page=page, total=len(results), per_page=page_size, search=False)
+    ##############  
+    if "free_search" in uri_raw:
+        return render_template('results_similar.html',  results=results_for_render, pagination=pagination,
+                                bar_plot=bar_plot, heatmap_plot=heatmap_plot)
+    else:
+        return render_template('results_similar.html', results=results_for_render, pagination=pagination, 
+                                term=term, definition=definition, uri=uri_raw, 
+                                enum=enum, year=year, vnum=vnum, t_name=t_name, 
+                                bar_plot=bar_plot, heatmap_plot=heatmap_plot, t_sentiment=t_sentiment)
+    
 
 @app.route("/topic_modelling", methods=["GET", "POST"])
 def topic_modelling(topic_name=None):
-    if request.method == "POST":
+    topic_name  = request.args.get('topic_name', None)
+    if topic_name == None:
         if 'topic_name' in request.form:
             topic_name=request.form.get('topic_name')
             if topic_name=="":
                 topic_name="5_see_dialling_villain_article"
-            indices = [i for i, x in enumerate(topics_names) if x == topic_name]
-            results={}
-            for t_i in indices:
-               score='%.2f'%sentiment_terms[t_i][0]['score']
-               sentiment = sentiment_terms[t_i][0]['label']+"_"+score
-               results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i], sentiment]
-            num_results=len(indices)
-            first_topic=topics[indices[0]]
-            fig1=topic_model.visualize_barchart([first_topic], n_words=10)
-            bar_plot = fig1.to_json()
-            return render_template('topic_modelling.html', topic_name=topic_name, results=results, bar_plot=bar_plot, num_results=num_results) 
-    topic_name  = request.args.get('topic_name', None)
-    if topic_name!=None:
-        indices = [i for i, x in enumerate(topics_names) if x == topic_name]
-        results={}
-        for t_i in indices:
-            score='%.2f'%sentiment_terms[t_i][0]['score']
-            sentiment = sentiment_terms[t_i][0]['label']+"_"+score
-            results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i], sentiment]
-        num_results=len(indices)
-        first_topic=topics[indices[0]]
-        fig1=topic_model.visualize_barchart([first_topic], n_words=10)
-        bar_plot = fig1.to_json()
-        return render_template('topic_modelling.html', topic_name=topic_name, results=results, bar_plot=bar_plot, num_results=num_results) 
+            session['topic_name'] = topic_name
+
+    if not topic_name:
+         topic_name=session.get('topic_name')
+    if not topic_name:
+        return render_template('topic_modelling.html')
+    indices = [i for i, x in enumerate(topics_names) if x == topic_name]
+    results={}
+    for t_i in indices:
+        score='%.2f'%sentiment_terms[t_i][0]['score']
+        sentiment = sentiment_terms[t_i][0]['label']+"_"+score
+        results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i], sentiment]
+    num_results=len(indices)
+    first_topic=topics[indices[0]]
+    fig1=topic_model.visualize_barchart([first_topic], n_words=10)
+    bar_plot = fig1.to_json()
+
+    #### Pagination ###  
+    page = int(request.args.get("page", 1))
+    page_size=10
+    per_page = 10
+    offset = (page-1) * per_page
+    limit = offset+per_page
+    results_for_render=dict(islice(results.items(),offset, limit))
+    pagination = Pagination(page=page, total=len(results), per_page=page_size, search=False)
+    ##############  
+
+    return render_template('topic_modelling.html', topic_name=topic_name, 
+                                    results=results_for_render, pagination=pagination, 
+                                    bar_plot=bar_plot, num_results=num_results) 
             
-    return render_template('topic_modelling.html')
 
 @app.route("/topic_summarization", methods=["GET"])
 def topic_summarization():
