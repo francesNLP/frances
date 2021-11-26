b85cd251ca22c83f5b4b8817398a9cb174834470
rfilguei <r.filgueira@epcc.ed.ac.uk>
2021-11-26 14:48:25 +0000

    updates

--- web-app/query_app/controllers.py
+++ web-app/query_app/controllers.py
@@ -7,7 +7,7 @@ from flask_paginate import Pagination, get_page_parameter
 import itertools
 from itertools import islice
 from sklearn.metrics.pairwise import cosine_similarity
-from .utils import calculating_similarity_text, get_topic_name
+from .utils import calculating_similarity_text, get_topic_name, load_data
 
 import numpy as np
 import os
@@ -17,36 +17,27 @@ from tqdm import tqdm
 from bertopic import BERTopic
 
 ########
-input_path="/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models"
-text_embeddings = np.load('/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models/embeddings_1ed.npy')
+input_path_embed="/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models/all-mpnet-base-v2"
+input_path="/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models/"
 
-with open (os.path.join(input_path, 'similarities_1ed.txt'), 'rb') as fp:
-    similarities = pickle.load(fp)
+text_embeddings = np.load('/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models/all-mpnet-base-v2/embeddings_1ed.npy')
+topic_model = BERTopic.load("/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models/all-mpnet-base-v2/BerTopic_Model_1ed") 
 
-with open (os.path.join(input_path, 'similarities_sorted_1ed.txt'), 'rb') as fp1:
-    similarities_sorted = pickle.load(fp1)
+similarities=load_data(input_path_embed, 'similarities_1ed.txt')
+similarities_sorted=load_data(input_path_embed, 'similarities_sorted_1ed.txt')
+documents=load_data(input_path, 'documents_1ed.txt')
+terms_info=load_data(input_path, 'terms_info_1ed.txt')
+uris=load_data(input_path, 'uris_1ed.txt')
+topics=load_data(input_path_embed, 'topics_1ed.txt')
+topics_name=load_data(input_path_embed, 'topics_names_1ed.txt')
+topics_names=load_data(input_path_embed, 'topics_names_1ed.txt')
+summary_1771=load_data(input_path,'topics_summary_1771.txt') 
+sentiment_terms=load_data(input_path,'sentiment_documents_1ed.txt') 
 
-with open (os.path.join(input_path, 'documents_1ed.txt'), 'rb') as fp2:
-    documents = pickle.load(fp2)
+#model = SentenceTransformer('bert-base-nli-mean-tokens')
+#model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('all-mpnet-base-v2')
 
-with open (os.path.join(input_path, 'terms_info_1ed.txt'), 'rb') as fp3:
-    terms_info = pickle.load(fp3)
-
-with open (os.path.join(input_path, 'uris_1ed.txt'), 'rb') as fp4:
-    uris = pickle.load(fp4)
-
-with open (os.path.join(input_path, 'topics_1ed.txt'), 'rb') as fp5:
-    topics = pickle.load(fp5)
-
-with open (os.path.join(input_path, 't_names_1ed.txt'), 'rb') as fp6:
-    t_names = pickle.load(fp6)
-
-with open (os.path.join(input_path, 'topics_names_1ed.txt'), 'rb') as fp7:
-    topics_names = pickle.load(fp7)
-
-model = SentenceTransformer('bert-base-nli-mean-tokens')
-
-topic_model = BERTopic.load("/Users/rosafilgueira/HW-Work/NLS-Fellowship/work/frances/web-app/models/BerTopic_Model_1ed") 
 
 ######
 
@@ -58,7 +49,7 @@ def home_page():
 @app.route("/term_search",  methods=['GET', 'POST'])
 def term_search(termlink=None):
     
-    headers=["Year", "Edition", "Volume", "Start Page", "End Page", "Term Type", "Definition", "Related Terms", "Topic Modelling"]
+    headers=["Year", "Edition", "Volume", "Start Page", "End Page", "Term Type", "Definition", "Related Terms", "Topic Modelling", "Sentiment_Score"]
     if request.method == "POST":
         if "search" in request.form:
             term = request.form["search"]
@@ -79,10 +70,13 @@ def term_search(termlink=None):
     for key, value in results.items():
         index_uri=uris.index(key)
         topic_name = topics_names[index_uri]
+        score='%.2f'%sentiment_terms[index_uri][0]['score']
+        sentiment = sentiment_terms[index_uri][0]['label']+"_"+score
         if topics[index_uri] not in topics_vis:
             topics_vis.append(topics[index_uri])
         #topic_name = get_topic_name(index_uri, topics, topic_model)
         value.append(topic_name)
+        value.append(sentiment)
     if len(topics_vis) >= 1:
         fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
         bar_plot = fig1.to_json() 
@@ -178,7 +172,7 @@ def similar_terms(termlink=None):
                 uri_raw="https://w3id.org/eb/i/Article/992277653804341_144133901_ABACISCUS_0"
             else:
                 uri_raw=""
-                results, topics_vis=calculating_similarity_text(data_similar,text_embeddings, model, terms_info, documents,uris, topics_names,topics)              
+                results, topics_vis=calculating_similarity_text(data_similar,text_embeddings, model, terms_info, documents,uris, topics_names,topics, sentiment_terms)              
                 if len(topics_vis) >= 1:
                     fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
                     bar_plot = fig1.to_json() 
@@ -194,15 +188,19 @@ def similar_terms(termlink=None):
                 term, definition, enum, year, vnum  =get_document(uri)
                 index_uri=uris.index(uri_raw)
                 t_name=topics_names[index_uri]
+                score='%.2f'%sentiment_terms[index_uri][0]['score']
+                t_sentiment = sentiment_terms[index_uri][0]['label']+"_"+score
                 results={}
                 topics_vis=[]
                 for i in range(-2, -12, -1):
                     similar_index=similarities_sorted[index_uri][i]
                     rank=similarities[index_uri][similar_index]
+                    score='%.2f'%sentiment_terms[similar_index][0]['score']
+                    sentiment = sentiment_terms[similar_index][0]['label']+"_"+score
                     topic_name = topics_names[similar_index]
                     if topics[similar_index] not in topics_vis:
                         topics_vis.append(topics[similar_index])
-                    results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank]
+                    results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank, sentiment]
                 if len(topics_vis) >= 1:
                     fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
                     bar_plot = fig1.to_json()
@@ -217,7 +215,7 @@ def similar_terms(termlink=None):
             if uri_raw == "":
                 return render_template('similar.html', results=results, bar_plot=bar_plot, heatmap_plot=heatmap_plot)
             else:
-                return render_template('similar.html', results=results, term=term, definition=definition, uri=uri_raw, enum=enum, year=year, vnum=vnum, t_name=t_name, bar_plot=bar_plot, heatmap_plot=heatmap_plot)
+                return render_template('similar.html', results=results, term=term, definition=definition, uri=uri_raw, enum=enum, year=year, vnum=vnum, t_name=t_name, bar_plot=bar_plot, heatmap_plot=heatmap_plot, t_sentiment=t_sentiment)
     
     termlink  = request.args.get('termlink', None)
     termtype  = request.args.get('termtype', None)
@@ -229,15 +227,19 @@ def similar_terms(termlink=None):
         uri_raw=uri.replace("<","").replace(">","")
         index_uri=uris.index(uri_raw)
         t_name=topics_names[index_uri]
+        score='%.2f'%sentiment_terms[index_uri][0]['score']
+        t_sentiment = sentiment_terms[index_uri][0]['label']+"_"+score
         results={}
         topics_vis=[]
         for i in range(-2, -12, -1):
             similar_index=similarities_sorted[index_uri][i]
             rank=similarities[index_uri][similar_index]
+            score='%.2f'%sentiment_terms[similar_index][0]['score']
+            sentiment = sentiment_terms[similar_index][0]['label']+"_"+score
             topic_name = topics_names[similar_index]
             if topics[similar_index] not in topics_vis:
                 topics_vis.append(topics[similar_index])
-            results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank]
+            results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank, sentiment]
 
         if len(topics_vis) >= 1:
             fig1=topic_model.visualize_barchart(topics_vis, n_words=10)
@@ -249,7 +251,7 @@ def similar_terms(termlink=None):
             heatmap_plot = fig2.to_json()
         else:
             heatmap_plot = None
-        return render_template('similar.html', results=results, term=term, definition=definition, uri=uri, enum=enum, year=year, vnum=vnum, t_name=t_name, bar_plot=bar_plot, heatmap_plot=heatmap_plot)
+        return render_template('similar.html', results=results, term=term, definition=definition, uri=uri, enum=enum, year=year, vnum=vnum, t_name=t_name, bar_plot=bar_plot, heatmap_plot=heatmap_plot, t_sentiment=t_sentiment)
     return render_template('similar.html')
 
 @app.route("/topic_modelling", methods=["GET", "POST"])
@@ -258,11 +260,13 @@ def topic_modelling(topic_name=None):
         if 'topic_name' in request.form:
             topic_name=request.form.get('topic_name')
             if topic_name=="":
-                topic_name="6_see_dialling_mahometans_bildge"
+                topic_name="5_see_dialling_villain_article"
             indices = [i for i, x in enumerate(topics_names) if x == topic_name]
             results={}
             for t_i in indices:
-               results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i]]
+               score='%.2f'%sentiment_terms[t_i][0]['score']
+               sentiment = sentiment_terms[t_i][0]['label']+"_"+score
+               results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i], sentiment]
             num_results=len(indices)
             first_topic=topics[indices[0]]
             fig1=topic_model.visualize_barchart([first_topic], n_words=10)
@@ -273,7 +277,9 @@ def topic_modelling(topic_name=None):
         indices = [i for i, x in enumerate(topics_names) if x == topic_name]
         results={}
         for t_i in indices:
-            results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i]]
+            score='%.2f'%sentiment_terms[t_i][0]['score']
+            sentiment = sentiment_terms[t_i][0]['label']+"_"+score
+            results[uris[t_i]]=[terms_info[t_i][1],terms_info[t_i][2], terms_info[t_i][4], terms_info[t_i][0], documents[t_i], sentiment]
         num_results=len(indices)
         first_topic=topics[indices[0]]
         fig1=topic_model.visualize_barchart([first_topic], n_words=10)
--- web-app/query_app/utils.py
+++ web-app/query_app/utils.py
@@ -1,6 +1,12 @@
 from tqdm import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+import os
 
+def load_data(input_path_embed, file_name):
+    with open (os.path.join(input_path_embed, file_name), 'rb') as fp:
+        data = pickle.load(fp)
+    return data
 
 def get_topic_name(index_uri, topics, topic_model):
     topic_num=topics[index_uri]
@@ -16,7 +22,7 @@ def get_topic_name(index_uri, topics, topic_model):
     topic_name=str(topic_num)+topic_name
     return topic_name
 
-def calculating_similarity_text(definition, text_embeddings, model, terms_info, documents, uris, topics_names, topics):
+def calculating_similarity_text(definition, text_embeddings, model, terms_info, documents, uris, topics_names, topics, sentiment_terms):
     definition_embedding= model.encode(definition, batch_size = 8, show_progress_bar = True)
     similarities=cosine_similarity( [definition_embedding], text_embeddings)
     similarities_sorted = similarities.argsort()
@@ -25,9 +31,11 @@ def calculating_similarity_text(definition, text_embeddings, model, terms_info, 
     for i in range(-1, -12, -1):
         similar_index=similarities_sorted[0][i]
         rank=similarities[0][similar_index]
+        score='%.2f'%sentiment_terms[similar_index][0]['score']
+        sentiment = sentiment_terms[similar_index][0]['label']+"_"+score
         topic_name = topics_names[similar_index]
         if topics[similar_index] not in topics_vis:
             topics_vis.append(topics[similar_index])
-        results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank]
+        results[uris[similar_index]]=[terms_info[similar_index][1],terms_info[similar_index][2], terms_info[similar_index][4], terms_info[similar_index][0], documents[similar_index], topic_name, rank, sentiment]
     return results, topics_vis
 
